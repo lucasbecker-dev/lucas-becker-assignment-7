dc040d7b26fc2f5d7cc2448d17d04634003e89ab
Lucas Becker <lucasbecker.dev@gmail.com>
2024-03-28 16:36:06 -0400

    removed needless test printing in main

--- src/com/coderscampus/assignment7/Assignment7Application.java
+++ src/com/coderscampus/assignment7/Assignment7Application.java
@@ -2,7 +2,5 @@ package com.coderscampus.assignment7;
 
 public class Assignment7Application {
     public static void main(String[] args) {
-        CustomList<Integer> testList1 = new CustomArrayList<>();
-        testList1.add(0, 50);
     }
 }
